08e964c9efb7b8204ea9e567408e410446b67b8a
阿部直也 <n.abe@gemcook.com>
2020-04-16 07:12:13 +0900

    [fupdate] finish section12 database

--- section12/lesson146.py
+++ section12/lesson146.py
@@ -26,3 +26,47 @@
         - disable 'table'
         - drop 'table'
 """
+
+# pythonのコードでhbaseを操る
+# $ hbase thrift start
+# $ pip install happybase
+# $ pip freeze | grep happybase   出力：happybase==1.2.0
+
+import happybase
+
+# コネクションの作成
+connection = happybase.Connection('localhost')
+
+# コネクションの開通
+connection.open()
+
+# テーブル作成
+connection.create_table(b'sns', {'blog': dict()})
+
+# テーブルとコネクションの接続
+table = connection.table(b'sns')
+
+# テーブルにデータを追加
+table.put(
+    b'user1', {
+        b'blog:bitcoin': b'user1 about bitcoin',
+        b'blog:soccer': b'user1 about soccer'
+    }
+)
+
+table.put(
+    b'user2', {
+        b'blog:soccer': b'user2 about soccer'
+    }
+)
+
+# テーブルの情報取得
+print(list(table.scan()))
+print()
+print(list(table.scan(row_prefix=b'user1')))
+print()
+print(list(table.scan(columns=[b'blog:soccer'])))
+
+# テーブルの削除
+connection.disable_table(b'sns')
+connection.delete_table(b'sns')
