53d7f022612917dc2387b721247d6ec242f433cc
阿部直也 <n.abe@gemcook.com>
2020-04-18 11:20:22 +0900

    [update] finish lesson157 Flask

--- section13/lesson157/lesson157.py
+++ section13/lesson157/lesson157.py
@@ -20,14 +20,63 @@ def get_db():  # FlaskのDB接続パターン
     return db
 
 # デコレーターがまだ理解できていない
-@app.teardown_appcontext　  # アプリケーションが終了する時に必ず実行する
+# アプリケーションが終了する時に必ず実行する
+@app.teardown_appcontext
 def close_connection(exception):
     db = getattr(g, '_database', None)
     # dbとのコネクションが存在したら、closeする
     if db is not None:
         db.close()
 
-# TODO DBを使った関数の作成
+# DBを使った関数の作成
+@app.route('/employee', methods=['POST', 'PUT', 'DELETE'])
+@app.route('/employee/<name>', methods=['GET'])
+def employee(name=None):
+    # DBに接続
+    db = get_db()
+    curs = db.cursor()
+    # テーブルを作成
+    curs.execute(
+        'CREATE TABLE IF NOT EXISTS persons(id INTEGER PRIMARY KEY AUTOINCREMENT, name STRING)')
+    db.commit()
+
+    # URLから値を取得
+    name = request.values.get('name', name)
+
+    # GET
+    if request.method == 'GET':
+        # nameが入力されたら、そのnameでdb内を検索しにいく
+        curs.execute('SELECT * FROM persons WHERE name = "{}"'.format(name))
+        person = curs.fetchone()
+        if not person:
+            return "No", 404
+        user_id, name = person
+        # 結果と同時にステータスコードも返す
+        return 'get {}:{}'.format(user_id, name), 200
+
+    # POST
+    if request.method == 'POST':
+        # personsテーブルのnameカラムに入力された値を記録
+        curs.execute('INSERT INTO persons(name) values("{}")'.format(name))
+        db.commit()
+        return 'created {}'.format(name), 200
+
+    # PUT
+    if request.method == 'PUT':
+        # 値がなければエラーが返る　request.values.get()だと入力値がない場合は「None」が返される
+        new_name = request.values['new_name']
+        curs.execute('UPDATE persons set name = "{}" WHERE name = "{}"'.format(
+            new_name, name))
+        db.commit()
+        return 'updated {}:{}'.format(name, new_name), 200
+
+    # DELETE
+    if request.method == 'DELETE':
+        curs.execute('DELETE from persons WHERE name = "{}"'.format(name))
+        db.commit()
+        return 'delete {}'.format(name), 200
+
+    curs.close()
 
 # landingページでは、hello_world()を実行する
 @app.route('/')
--- section13/lesson157/test_flask.py
+++ section13/lesson157/test_flask.py
@@ -1,15 +1,19 @@
 import requests
 
-# r = requests.post(
-#     'http://127.0.0.1:5000/post', data={'username': 'mike'}
-# )
+r = requests.get('http://127.0.0.1:5000/employee/naoya')
+print(r)
+
+r = requests.post(
+    'http://127.0.0.1:5000/employee', data={'name': 'naoya'}
+)
+print(r.text)
 
 r = requests.put(
-    'http://127.0.0.1:5000/post', data={'username': 'mike'}
+    'http://127.0.0.1:5000/employee', data={'name': 'naoya', 'new_name': 'abe'}
 )
+print(r.text)
 
-# r = requests.delete(
-#     'http://127.0.0.1:5000/post', data={'username': 'mike'}
-# )
-
+r = requests.delete(
+    'http://127.0.0.1:5000/employee', data={'name': 'abe'}
+)
 print(r.text)
